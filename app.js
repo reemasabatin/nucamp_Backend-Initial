fb7f79e6b43a89bd8e35f38b7f4a4ccf5c23d311
Reema Sabatin <rmsabatin@up.edu.ph>
2022-05-19 11:39:18 +0800

    Start of Week 1 Exaple Javascript Program

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-const rect = require("./rectangle");
+const rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y,
+};
 
 function solveRect(l, w) {
   console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
-  rect(l, w, (err, rectangle) => {
-    if (err) {
-      console.log("ERROR:", err.message);
-    } else {
-      console.log(
-        `Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`
-      );
-      console.log(
-        `Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`
-      );
-    }
-  });
-  console.log("This statement is logged after the call to rect()");
+  if (l <= 0 || w <= 0) {
+    console.log(
+      `Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`
+    );
+  } else {
+    console.log(`Area of Rectangle: ${rect.area(l, w)}`);
+    console.log(`Perimeter of Rectangle: ${rect.perimeter(l, w)}`);
+  }
 }
 
 solveRect(2, 4);
 solveRect(3, 5);
-solveRect(0, 5);
 solveRect(5, -3);
